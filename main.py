087c1da13de4ab0a32194c1d3cb888dc65c2250b
shashank-2102 <shashank.prabhu21@gmail.com>
2024-11-04 17:25:30 +0100

    Basic swarm implementation

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("ye nan")
+from swarm import Swarm, Agent
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#api_key = os.getenv("OPENAI_API_KEY")
+
+
+client = Swarm()
+
+
+def transfer_to_agent_b():
+    return agent_b
+
+
+agent_a = Agent(
+    name="Agent A",
+    instructions="You are a helpful agent.",
+    functions=[transfer_to_agent_b],
+)
+
+agent_b = Agent(
+    name="Agent B",
+    instructions="Only speak in Haikus.",
+)
+
+response = client.run(
+    agent=agent_a,
+    messages=[{"role": "user", "content": "I want to talk to agent B."}],
+)
+
+print(response.messages[-1]["content"])
